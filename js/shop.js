7c68869724d9cba5f11b4562ad6592ed3e8aedaf
anhtuan <phananhtuan662004@gmail.com>
2024-12-20 17:31:53 +0700

    sua shop

---
+++ js/shop.js
@@ -0,0 +1,33 @@
+
+    
+    // Lấy phần tử đầu tiên có class 'Pic-1'
+    let firstImg1 = document.getElementsByClassName('Pic-1')[0];
+    firstImg1.addEventListener('mouseover', function() {
+        firstImg1.src = "../../Picture/products-8-600x600.jpg";
+    });
+    firstImg1.addEventListener('mouseout', function() {
+        firstImg1.src = "../../Picture/products-7-600x600.jpg";
+    });
+    
+    // -----------------------------------------------
+    
+    // Lấy phần tử đầu tiên có class 'Pic-2'
+    let firstImg_11 = document.getElementsByClassName('Pic-2')[0];
+    firstImg_11.addEventListener('mouseover', function() {
+        firstImg_11.src = "../../Picture/img-16-600x600.jpg";
+    });
+    firstImg_11.addEventListener('mouseout', function() {
+        firstImg_11.src = "../../Picture/img-15-9-600x600.jpg";
+    });
+    
+    // -----------------------------------------------
+    
+    // Lấy phần tử đầu tiên có class 'Pic-3'
+    let firstImg_21 = document.getElementsByClassName('Pic-3')[0];
+    firstImg_21.addEventListener('mouseover', function() {
+        firstImg_21.src = "../../Picture/products-2-600x600.jpg";
+    });
+    firstImg_21.addEventListener('mouseout', function() {
+        firstImg_21.src = "../../Picture/products-1-600x600.jpg";
+    });
+    
